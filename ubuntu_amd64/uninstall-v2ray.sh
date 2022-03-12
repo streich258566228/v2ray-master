6044198887977891675b6dc11fbd0227a0bfc414
StReIcH <38107701+streich258566228@users.noreply.github.com>
2022-03-12 12:44:05 +0800

    Delete uninstall-v2ray.sh

--- ubuntu_amd64/uninstall-v2ray.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo systemctl stop v2ray
-sudo systemctl disable v2ray
-sudo rm -r /var/log/v2ray /usr/local/share/v2ray /usr/local/etc/v2ray /etc/systemd/system/v2ray.service.d /etc/systemd/system/v2ray@.service.d
-sudo rm /etc/systemd/system/v2ray.service /etc/systemd/system/v2ray@.service /usr/local/bin/v2ray /usr/local/bin/v2ctl
-echo "v2ray UNINSTALL SUCCESSFULLY!"
